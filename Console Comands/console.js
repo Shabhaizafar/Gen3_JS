a290ea4a60e382e87ad044cf4c73d19cb3395ac9
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-21 16:47:24 +0530

    Console

---
+++ Console Comands/console.js
@@ -0,0 +1,27 @@
+
+console.time("Total Time");
+//Print Normal Text 
+console.log("Zafar");
+console.log(1);
+
+
+
+console.log([12,1,2,6,123,16,19,3,4,5,6,7,8]);
+console.table([12,1,2,6,123,16,19,3,4,5,6,7,8]);
+
+
+console.error("This is an Error !!");
+console.error("Please Check Your Choice Selection");
+
+
+console.warn("This is a Warning");
+console.timeEnd("Total Time");
+
+console.time("Total Time");
+for (let i = 0; i < 1000; i++) {
+    console.log(1);
+}
+console.timeEnd("Total Time");
+
+
+console.clear();
