1541b282bcd2c5663508d4fe4fe7b5fd42e49b56
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-22 19:47:17 +0530

    Variables

---
+++ Variables/rule.js
@@ -0,0 +1,21 @@
+//Rule of variable name  : 
+// 1) we cann't start variable name with number
+    // 1num ,12     (invalid)
+// 2) we cann't une special  char in a variable name :   _ , $
+    //#num ,!num (invalid)
+    // _num ,$num   (valid)
+// 3) we cann't use blank space in a variable name
+    //first name (invalid)   first_name  (valid)
+// 4) we cann't use keyword name as a variable name
+    // var = 12 (invalid) 
+    // let = 12  (invalid)
+
+// 5) unique variable name.
+     // not try to repeat name again
+// 6) must use a camelCase 
+        // firstname  lowecase
+        // firstName  camelCase
+
+
+
+// https://www.javaguides.net/2023/07/javascript-variables-quiz.html
---
+++ Variables/variables.js
@@ -0,0 +1,53 @@
+// //Variables : 
+// //number,string,boolean,null,undefined,object
+
+
+// //number
+// console.log(typeof(12));
+// console.log(12);
+// console.log(typeof(12.14));
+// console.log(12.14);
+
+// //String
+// console.log(typeof "Zafar");
+// console.log("Zafar");
+// console.log(typeof 'A');
+// console.log("A");
+
+// //Boolean 
+// console.log(typeof(true));
+// console.log(true);
+// console.log(typeof(false));
+// console.log(false);
+
+// //undefined
+// console.log(typeof undefined);
+// console.log(undefined);
+
+
+// //Object 
+// console.log(typeof null);
+// console.log(null);
+// console.log(typeof [1,2,3,4]);
+// console.log([1,2,3,4]);
+
+// //typeof Operator
+////////////////////////////////////////////////
+// how to declare variable ? 
+//1.var
+//2.let
+//3.const
+/////////////////////////
+/using var Keyword/
+// var fname ="royal";
+// console.log(typeof fname);
+// console.log(fname);
+// var num = 12;
+// console.log(typeof num);
+// console.log(num);
+// var bool = true;
+// console.log(typeof bool);
+// console.log(bool);
+
+// var fname;//declaration
+// fname = "royal";//initialization 
