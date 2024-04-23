8d57f2ae2bce5225d89d9b07f2b21d14b2b5c48f
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-23 20:13:47 +0530

    String

---
+++ String/StringIntro.js
@@ -0,0 +1,52 @@
+//What is a String :
+// -  String is a Collection of Multiple Char.
+// -  String is not an Array.but it like an Array
+
+
+// using double Quote
+// var str = "Royal";
+// console.log(str);
+// console.log(typeof(str));
+
+// using single Quote
+// var str2 = 'Technosoft';
+// console.log(str2);
+// console.log(typeof(str2));
+
+// using back-tick
+// var str3 = `p ltd`;
+// console.log(str3);
+// console.log(typeof(str3));
+
+//length   123456...............
+// var str = "Royal Technosoft p ltd.";
+//index    01234..................
+// console.log(str);
+
+
+//Methods
+//1) length
+// console.log(str.length);
+
+
+// console.log(str[0]);
+// console.log(str[str.length-1]);
+
+
+
+
+// -  String is not an Array.but it like an Array
+// var str = "Royal";
+// var Arr = [1,2,3];
+// console.log(Array.isArray(str));
+// console.log(Array.isArray(Arr));
+
+
+
+
+//note-pen  
+//1)
+// https://quizzesforyou.com/quiz/aboutjs
+
+//2) 
+// https://quizzesforyou.com/quiz/constletvar
---
+++ Variables/variables2.js
@@ -0,0 +1,46 @@
+// var ,let , const   (keywords)
+
+
+
+// /C-code/
+// int a = 12;
+// char ch = 'a';
+// float n2 = 3.3;
+
+// var n1 = "Zafar";
+// let n2 = "Royal";
+// const n3 = "A";
+
+// console.log(n1);
+// console.log(n2);
+// console.log(n3);
+
+
+
+var n1 = 12;
+console.log("Value of N1:",n1);
+n1=100;
+console.log("Value of N1:",n1);
+var n1 = 1000;
+console.log("Value of N1:",n1);
+
+
+
+
+
+let n2 = 13;
+console.log("Value of N2:",n2);
+n2 = 200;
+console.log("Value of N2:",n2);
+// let n2 = 2000;
+
+
+
+const n3 = 14;
+console.log("Value of N3:",n3);
+// n3 = 300;   //error  // invalid
+// console.log("Value of N3:",n3);
+
+// const n3 = 3000;
+
+
