002448cf1d1f75b4c87d0f14b7f448c02b0f7721
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-24 20:00:26 +0530

    String Methods

---
+++ String/Methods.js
@@ -0,0 +1,68 @@
+// var str = "Royal Technosoft p ltd";
+// console.log(str);
+
+
+//1) str.length
+// console.log(str.length);
+
+//2) str.charAt
+// console.log(str.charAt(0));
+
+//3) str.charCodeAt
+// console.log(str.charCodeAt(0));
+
+
+//4) str.concat
+// console.log(str.concat(" Zafar"," A"));
+
+//5) str.endsWith
+// console.log(str.endsWith('D'));//true or false
+// console.log(str.endsWith('lTd'));
+
+//6) str.startsWith
+// console.log(str.startsWith("RoyAl"));
+
+//7) str.includes
+// console.log(str.includes("Tech"));
+
+
+//8) str.indexOf
+// console.log(str.indexOf('z'));  // index else -1
+
+//9) str.lastIndexOf
+// console.log(str.lastIndexOf('z'));// index else -1
+
+//10) str.replace
+// console.log(str.replace('os','AA'));
+
+//11) str.replaceAll
+// console.log(str.replaceAll('o','A'));
+
+//12) str.toLowerCase
+// console.log(str.toLowerCase());
+
+
+//13) str.toUpperCase
+// console.log(str.toUpperCase());
+
+//14) str.trim
+// console.log(str.trim());
+
+//15) str.trimEnd
+// console.log(str.trimEnd());
+
+//16)  str.trimStart
+// console.log(str.trimStart());
+
+//17) str.valueOf
+// console.log(str.valueOf());  // X
+
+
+
+
+// str.split
+
+// str.slice
+// str.substring
+// str.substr 
+
