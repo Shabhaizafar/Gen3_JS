7d8f48e091fd163457233b323532dbc291719d82
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-11 15:56:53 +0530

    Array

---
+++ Array/Array1.js
@@ -0,0 +1,61 @@
+// - Intro to arrays : 
+    //  [1,2,3,4]
+    //  ["Zafar","Z","A","F"]
+
+    // Collection of Multiple Data with Diffrent Datatype.
+    //[1,2,3] valid
+    //["zafar","Z","A"] valid
+    // [1,2,"Zafar"]
+
+    // index :  starting 0
+
+
+
+// var Arr = []; // Empty Array
+
+// console.log(Array.isArray(Arr));
+
+//length   1 2 3 4 5 6 7 8 9 
+// var Arr = [1,2,3,4,5,6,7,8,9,"Zafar",true];
+// //index    0 1 2 3 4 5 6 7 8 
+
+// console.log(Arr);
+// console.log(Arr[3]);
+
+// console.log(Arr[Arr.length-1]);
+
+////////////////////////////////////////////////
+// - Push pop shift unshift.
+// var Arr = [1,2,3,4,5];
+// console.log(Arr);
+
+
+// 1) Push()  : Add new Element in an Array (last Index)
+// Arr.push(100);
+// Arr.push(100,200,300);
+// console.log(Arr);
+
+
+// 2) pop() : Remove Element in an Array (last Index)
+// Arr.pop();
+// Arr.pop();
+// console.log(Arr);
+
+// 3) shift :  Remove Element in an Array (Starting Index)
+// Arr.shift();
+// console.log(Arr);
+
+
+// 3) unshift : Add new Element in an Array (Starting Index)
+// Arr.unshift(100);
+// Arr.unshift(100,200,300);
+
+// console.log(Arr);
+
+
+//////////////////////////////////////////////////
+
+// 1) Create an Array with diffrent Data type and print All values.
+// 2) Add new Element last postion using push and print an Array.
+// 3) Add new Element starting position using unshft and print an array.
+// 4) remove 2 values starting and ending index
