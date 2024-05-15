d1f8c95b0ba3790ce91539a0154865e91c6104c3
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-15 19:49:58 +0530

    Object

---
+++ Array/Array4.js
@@ -0,0 +1,43 @@
+// - use const for creating arrays.
+// const n1 = 12;
+// n1 = 13;
+
+
+// const Arr = [1,2,3];
+// console.log(Arr);
+
+// Arr.push(100);
+// console.log(Arr);
+
+// Arr = [];
+
+// var Arr = [1,2,3];
+// console.log(Arr);
+
+// Arr.push(100);
+// console.log(Arr);
+
+// Arr = [];
+// console.log(Arr);
+
+
+////////////////////////////////////////////////
+// - Array destructuring.
+// const Arr = [1,2,3,4,5,6,7,8,9,0];
+// console.log(Arr);
+
+// // var [v14,v20,,v40,...newArr] = Arr;
+
+// var [v14,v20,v40,...newArr] = Arr;
+
+// console.log(v14)
+// console.log(v20)
+// console.log(v40)
+// console.log(newArr);
+
+// var n2 = 12
+
+/////////////////////////////////////////
+
+// var Arr = [..."1234567890"];
+// console.log(Arr)
---
+++ Object/Object1.js
@@ -0,0 +1,19 @@
+// - Intro to objects.
+// - collection of Multiple Properties and Methods.
+    // - pair (keys values)
+
+
+// How to Create a Empty Object
+// var Person = {};
+// console.log(Person,typeof Person);
+
+
+var Person = {
+//   key      value    =  property
+    fname : "Raj",
+    lname : "Shah",
+    Age : 12
+}
+
+//How to Access All Data an Object
+console.log(Person);
