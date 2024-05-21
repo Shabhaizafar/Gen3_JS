76e6adca64ae222d667429ee6d0a4d076e4a6adc
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-21 16:44:36 +0530

    Backup

---
+++ Backup/All.js
@@ -0,0 +1,89 @@
+// ✅ Hello world program.
+
+//main screen
+// document.write("Hello");
+
+// document.write("<h1>Hello</h1>");
+
+
+//console screen  (Testing/ Debugging)
+// console.log("Hello");
+// // console.log("H");
+// console.log("\n\t<h1>aert</h1>");
+
+// \n : new line  \t : extra space 
+// console.time("Time");
+// console.log([1,2,3,4]);
+// console.table([1,13,2,3,4]);
+// console.error("This is an Error");
+// console.warn("This is a warning");
+// console.timeEnd("Time");
+
+
+///////////////////////////////////////////////
+// ✅ Declare variable using var.
+// n1 = 12;
+// var s1 = "Zafar";
+// var c1 = 'c';
+// var Dhruv = true;
+// console.log(n1);
+// console.log(c1);
+// console.log(s1);
+// console.log(Dhruv);
+
+// Number : 1,2,1.2..................   2^53
+// Bigint : bigint
+// String : "asdfg",'s'
+// Boolean : true/false
+// undefined : undefined
+// object : object/array/set/map/null............
+// function : function
+
+///////////////////////////////////////////
+// ✅ More about variable.
+// always start alphabet or _ or $ ..............
+// don't use space/comma/desh 
+// cammelCase
+// firstname   firstName
+/////////////////////////////////////////
+// ✅ Let.
+// ✅ Const.
+
+var n1 = 12;
+// n1=100;
+var n1 = 1000;
+console.log(n1);
+// we can re-initialization 
+// we can re-declaration
+
+let n2 = 13;
+// n2=200;
+// let n2 = 2000;
+console.log(n2);
+// we can re-initialization 
+// we cann't re-declaration
+
+const n3 = 14;
+// n3=300;// error
+console.log(n3);
+// we cann't re-initialization 
+// we cann't re-dclaration
+//////////////////////////////////////////
+// ✅ String Indexing.
+// ✅ Useful string methods.
+// ✅ Template Strings.
+// ✅ Null, undefined, BigInt, typeof.
+// ✅ Booleans and Comparison Operator.
+// ✅ Truthy and Falsy Values.
+// ✅ If else statement.
+// ✅ Ternary Operator.
+// ✅ && || operator.
+// ✅ Nested if else.
+// ✅ If elseif else.
+// ✅ Switch statement.
+// ✅ While loop.
+// ✅ While loop examples.
+// ✅ For loop.
+// ✅ For loop examples.
+// ✅ Break and continue keyword.
+// ✅ Do while loop.
