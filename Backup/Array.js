805f2af2eb4fc21e3e213c40e0d0a6a1d5f71557
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-22 19:54:32 +0530

    Al;

---
+++ Backup/Array.js
@@ -0,0 +1,75 @@
+
+
+// Intro to arrays.
+// Array is Collection of Multiple Data with same or diffrent datatype
+// Array index based 
+// int Arr[] = {1,2,3,4}
+// var Arr = [1,2,32,1,,2,4,56,7,9];
+//index    0 1 2
+// console.log(Arr);
+// var Arr2 = [1,2,"Royal"];
+// console.log(Arr2);
+// console.log(Arr[12]);
+
+//property length : 
+// console.log(Arr.length);
+
+// console.log(Arr[Arr.length-1]);
+
+// var Arr1 = [1,2,[1,2]];
+// var Arr2 = [4,5,6];
+// console.log(Arr1+Arr2); //1,2,34,5,6  //string    //1,2,1,24,5,6
+////////////////////////////////////////////////////////////////
+// ✅ 
+//  Push  : Add new Element at last index ( Multiple Value possible)
+//  pop : remove an Element  at Last index (not Possible for Multiple)
+//  shift  : remove an Element  at First index (not Possible for Multiple)
+//  unshift  : Add new Element at starting index(0) ( Multiple Value possible)
+
+// var Arr = [1,2,3,4];
+// console.log(Arr);
+// Arr.push(100);
+// Arr.push(100,200,300);
+// console.log(Arr);
+
+// Arr.pop();
+// Arr.pop();
+
+// Arr.shift();
+// Arr.shift();
+
+// Arr.unshift(100);
+// Arr.unshift(100,200,300);
+// console.log(Arr);
+
+// delete Arr[0];
+// console.log(Arr);
+// console.log(typeof Arr[0]);
+
+
+
+
+
+////////////////////////////////////////////
+// ✅ Primitive vs reference data types.
+//Primitive Data type : 
+// Number,Boolean,String 
+
+//Refference Datatype : 
+//object,function
+
+// var Arr = [1,2,3];
+
+
+
+//////////////////////////////////////////
+// ✅ Clone array & spread operator.
+
+
+/////////////////////////////////////////
+// ✅ For loop.
+// ✅ use const for creating arrays.
+// ✅ While loop in array.
+// ✅ For of loop.
+// ✅ For in loop.
+// ✅ Array destructuring.
