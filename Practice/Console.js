2954f6f82db1cef5ab87a56eafe83e82f33e8280
Shabhaizafar <zafarShabhai03@gmail.com>
2024-06-10 17:49:03 +0530

    Practice

---
+++ Practice/Console.js
@@ -0,0 +1,72 @@
+// **Print Hello World**
+// console.log("Hello World!!");
+
+// 2. **Print a Number**
+// console.log(12);
+
+// 3. **Print a Variable Value**
+// var n1 = 12;
+// console.log(n1);
+
+// 4. **Print the Sum of Two Numbers**
+// console.log(12+13);
+
+// 5. **Print a Concatenated String**
+// console.log("Hello"+"World!");
+// console.log("Hello".concat("World!"));
+
+// 6. **Print the Length of a String**
+// console.log("Hello Everyone".length);
+
+// 7. **Print an Array**
+// console.log([1,2,3,4]);
+
+// 8. **Print the First Element of an Array**
+// console.log([1,2,3,4][0]);
+// console.log([1,2,3,4][1,0]);
+
+// 9. **Print a Boolean Value**
+// console.log(true);
+
+// 10. **Print an Object**
+// console.log({fname : "Royal",locations : "GNR"});
+
+// 11. **Print the Value of an Object Property**
+// console.log({fname : "Royal",locations : "GNR"}.fname);
+// console.log({fname : "Royal",locations : "GNR"}['locations']);
+
+
+// 12. **Print a Mathematical Expression**
+// var x = 5;
+// console.log(2*x + 10);
+
+// 13. **Print a Function Call Result**
+// (function(){console.log("Hello")})();
+
+// 14. **Print a Random Number**
+// console.log(parseInt(Math.random()*11));
+
+
+// 15. **Print the Current Date and Time**
+// var Today = new Date();
+// console.log(Today);
+
+// 16. **Print the Type of a Variable**
+// console.log(typeof 12);
+// console.log(typeof(12));
+
+
+// 17. **Print a Null Value**
+// console.log(null);
+
+// 18. **Print Undefined Value**
+// console.log(undefined);
+// var n1;
+// console.log(n1);
+
+
+// 19. **Print a String in Uppercase**
+// console.log("HeLlo".toUpperCase());
+
+// 20. **Print a String in Lowercase**
+// console.log("HeLlo".toLowerCase());
