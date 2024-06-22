43b9f37e5638658250e3c0e1d80a69d314c48971
Shabhaizafar <zafarShabhai03@gmail.com>
2024-06-22 15:29:57 +0530

    Basic

---
+++ Practice/Basic6.js
@@ -0,0 +1,45 @@
+// 8. Write a function that checks if an object is empty (has no properties).
+// function All(obj){
+//     var count = 0;
+//     for (const key in obj) {
+//         count++;
+//     }
+//     if(count==0)
+//         console.log("Obj is empty");
+// }
+
+// All({});
+// 9. Check the truthiness of `NaN` and print the result.
+// console.log(Boolean(NaN)); //false
+
+// 10. Use logical `||` to assign a default value to a variable if another variable is falsy.
+
+// var n1 = 12;
+// var n2 = (!n1)? 1234 : undefined;
+// console.log(n1);
+
+// var n1 = null;
+// var n2 = 12;
+// if(!n1 || n2==undefined)
+// {
+//     n2 = 12345;
+// }
+// console.log(n2);
+
+
+// 5. Write a function that takes a value and returns a default value if it is falsy.
+
+// function All(v){
+//     var ans  = (v) ? v : 12;
+//     return ans; 
+// }
+// console.log(All(null));
+
+
+
+// 1. Write a function that checks if a number is positive, negative, or zero.
+// 2. Use `if-else` to check if a string contains more than 10 characters.
+// 3. Write a function that checks if a person is eligible to vote based on their age.
+// 4. Use `if-else` to print whether a number is a multiple of 3.
+// 5. Write a function that checks if a value is `null` or `undefined`.
+// 6. Use `if-else` to check if a number is even or odd.
