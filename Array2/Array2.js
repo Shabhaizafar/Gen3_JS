2afade2eda8490b3c590333db936d1422d91108b
Shabhaizafar <zafarshabhai03.com>
2024-07-15 17:42:32 +0530

    Array

--- Array2/Array2.js
+++ Array2/Array2.js
@@ -36,13 +36,41 @@
 // });
 // console.log(ans);
 
+//////////////////////////////////////////
+var Arr = [11,14,12,19,18,15,20];
 
 //    - Foreach method.
-//    - Map method.
-//    - Filter.
-//    - Find.
-//    - Reduce.
+//     //v = value / i = index / a = Array
+// var ans = Arr.forEach((v,i,a)=>{
+//     return v;
+// });
+// console.log(ans);
+
+
 //    - Fill method.
+// Arr.fill(0);
+// Arr.fill(0,2);
+// 0 :fill   2 : starting index  5 : ending index
+// Arr.fill(0,2,5);
+
+
+// console.log(Arr);
+////////////////////////////
+//    - Find.
+
+// var ans = Arr.find(
+//     (v)=>{
+//         return v==20;
+//     }
+// );
+// console.log(ans);
+
+
+
+///////////////////////////////////////
+//    - Filter.
+//    - Map method.
+//    - Reduce.
 //    - Splice method.
 
 
@@ -64,3 +92,22 @@
 // Check for Even Numbers: Write a function that checks if there are any even numbers in an array using the some() method.
 
 // String Starting with Specific Letter: Determine if there is any string in an array that starts with the letter 'A'.
+
+/////////////////////////////////////////////////////////
+
+// / 1. **Find:**
+//    - Write a function `findEvenIndex` that takes an array of integers and returns the index of the first even number using `Array.prototype.find`. If no even number is found, return `-1`.
+
+// 2. **Fill:**
+//    - Create an array of 5 elements and use `Array.prototype.fill` to replace all elements with a specific value (e.g., `'filled'`).
+// 5. **Find:**
+//    - Create a function `findPrime` that takes an array of integers and returns the first prime number using `Array.prototype.find`. If no prime number is found, return `null`.
+
+// 6. **Fill:**
+//    - Initialize an array of 8 elements with the numbers from 1 to 8 using `Array.prototype.fill` and a callback function.
+// 9. **Find:**
+//    - Implement a function `findLongestWord` that takes an array of strings and returns the longest word using `Array.prototype.find`. If there are multiple longest words, return the first one found.
+
+// 10. **Fill:**
+//     - Create an array of 10 elements and use `Array.prototype.fill` to replace all elements with the numbers from 1 to 10.
+
