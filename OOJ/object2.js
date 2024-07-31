52102dd8b3f5803aa3d6a19c5df688fe9e19b3da
Shabhaizafar <zafarshabhai03.com>
2024-07-31 19:47:48 +0530

    object2

---
+++ OOJ/object2.js
@@ -0,0 +1,43 @@
+//How to Create a Multiple Object using  Function
+// function All(fname,lname){
+//     var user = {};
+//     user.Firstname = fname;
+//     user.Lastname = lname;
+//     user.greet = function (){
+//         console.log(`Hello,Your Name is ${user.Firstname} ${user.Lastname}.`);
+//     }
+//     return user;
+// }
+
+// var obj5 = All("Raj","Shah");
+// var obj6 = All("Zafar","A");
+// console.log(obj5);
+// console.log(obj6);
+
+// obj5.greet();
+// obj6.greet();
+
+
+
+/////////////////////////////////////////////////////
+// var AllMethods = {
+//     greet : function (){
+//         console.log(`Hello,Your Name is ${this.Firstname} ${this.Lastname}.`);
+//     }
+// }
+
+// function All(fname,lname){
+//     var user = {};
+//     user.Firstname = fname;
+//     user.Lastname = lname;
+//     user.greet = AllMethods.greet;
+//     return user;
+// }
+
+// var obj5 = All("Raj","Shah");
+// var obj6 = All("Zafar","A");
+// console.log(obj5);
+// console.log(obj6);
+
+// obj5.greet();
+// obj6.greet();
