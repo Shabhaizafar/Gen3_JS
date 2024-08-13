e6f7789a706e77765a933a2e61be103394fdccdd
Shabhaizafar <zafarshabhai03.com>
2024-08-13 16:28:21 +0530

    Class,new,this

---
+++ OOJ/newthis.js
@@ -0,0 +1,34 @@
+//this : window object 
+
+
+// globally space
+// console.log(window);
+
+// var alpha = 12; //store :-> global space == window
+// console.log(alpha);
+// console.log(window.alpha);
+// console.log(window);
+
+
+// console.log(this);
+
+
+
+// A new empty object is created.    : {}
+// The new object’s internal ‘Prototype’ property (__proto__) is set the same as the prototype of the constructing function.
+// The ‘this’ variable is made to point to the newly created object. It binds the property that is declared with the ‘this’ keyword to the new object.
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Basic Constructor Function:
+//  How do you create a constructor function in JavaScript? Write a constructor function Person that initializes properties name and age.
+
+// Creating Instances: How do you create an instance of a constructor function? Using the Person constructor function from the previous question, create an instance representing a person named "Alice" who is 30 years old.
+
+// Adding Methods: How do you add a method to a constructor function's prototype? Write a method greet for the Person constructor that returns a greeting string like "Hello, my name is Alice."
+
+// Class Syntax: How do you define a class using the class syntax in JavaScript? Convert the Person constructor function to a class with a constructor that initializes name and age.
+
+// Class Methods: How do you add a method to a class in JavaScript? Using the Person class, add a method introduce that returns a string introducing the person.
+
