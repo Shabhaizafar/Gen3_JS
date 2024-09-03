ed0d1a0868f2ca6538b90a2b564daa1ca6529b9a
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-03 16:35:28 +0530

    DOM1

---
+++ DOM/DOM1.js
@@ -0,0 +1,10 @@
+// Using ID :
+console.log(document.getElementById('heading2'));
+
+
+// Using Class :
+console.log(document.getElementsByClassName('class1'));
+
+
+// Using Tagname : 
+console.log(document.getElementsByTagName('h1'));
